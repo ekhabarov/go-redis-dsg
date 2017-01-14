3745b06cd9ba7a07652e21bdcd53e68bbe7a11b8
Evgeny Khabarov <ekhabarov@dev.ms>
2017-01-15 00:26:28 +0300

    Made Consumer struct

--- consumer.go
+++ consumer.go
@@ -9,37 +9,96 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
-const MAX_RAND_PROCESS_TIME = 1000
+const (
+	MAX_RAND_PROCESS_TIME = 1000
 
-type Message string
+	CTRL_STOP = iota
+)
 
-//Waits for new messages by BRPOP
-func waiter(c redis.Conn, queue string, task chan<- Message) {
-	fmt.Println("Waiting for messages.")
-	for {
-		msg, err := c.Do("BRPOP", queue, 0)
-		if err != nil {
-			log.Println("unable to get message from redis: ", err)
-		}
+type (
+	Message string
 
-		v, err := redis.Values(msg, err)
-		PanicIf(err)
+	FinishedTask struct {
+		duration time.Duration
+		msg      Message
+		worker   int
+	}
 
-		s, err := redis.String(v[1], err)
-		PanicIf(err)
+	Consumer struct {
+		pool          *redis.Pool
+		queue         string
+		in            chan Message
+		out           chan FinishedTask
+		maxGoroutines int
+		control       chan byte
+	}
+)
 
-		task <- Message(s)
+func NewConsumer(p *redis.Pool, queue string, mg int) *Consumer {
+	return &Consumer{
+		pool:          p,
+		queue:         queue,
+		in:            make(chan Message),
+		out:           make(chan FinishedTask),
+		control:       make(chan byte),
+		maxGoroutines: mg,
 	}
 }
 
-//Make primary work
-func worker(wid int, tc <-chan Message, result chan<- finishedTask) {
+//Waits for new messages by BRPOP
+func (c *Consumer) Wait4Messages() {
+	fmt.Println("Waiting for messages. Run workers.")
+	for i := 1; i <= c.maxGoroutines; i++ {
+		go c.RunWorker(i)
+	}
+
+	for {
+		select {
+		case s := <-c.control:
+			switch s {
+			case CTRL_STOP:
+				fmt.Println("Workers stoped")
+				close(c.in)
+				return
+			default:
+				//Do nothing
+			}
+		default:
+			fmt.Printf("+")
+			pc := c.pool.Get()
+			defer pc.Close()
+
+			msg, err := pc.Do("BRPOP", c.queue, 0)
+			if err != nil {
+				log.Println("unable to get message from redis: ", err)
+			}
+
+			v, err := redis.Values(msg, err)
+			PanicIf(err)
+
+			s, err := redis.String(v[1], err)
+			PanicIf(err)
+
+			c.in <- Message(s)
+		}
+	}
+}
+
+//Makes primary work for random milliseconds.
+//Stop work by closing in channel
+func (c *Consumer) RunWorker(wid int) {
 	//fmt.Printf("Worker %d started.\n", wid)
+	fmt.Printf(".")
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	processTime := time.Duration(r.Intn(MAX_RAND_PROCESS_TIME))
 
-	for t := range tc {
+	for t := range c.in {
 		time.Sleep(time.Millisecond * processTime)
-		result <- finishedTask{processTime, t, wid}
+		c.out <- FinishedTask{processTime, t, wid}
 	}
+}
+
+func (c *Consumer) Stop() {
+	fmt.Println("Stop all workers")
+	c.control <- CTRL_STOP
 }
